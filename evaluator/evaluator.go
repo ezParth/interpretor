36d29dbd4672ef04b6cd6ab79c1111903511cb8b
ezParth <parthsingh55m@gmail.com>
2025-06-18 15:17:25 +0530

    added boolean evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -13,6 +13,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.Program:
 		return evalStatements(node.Statements)
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
 	}
 	return nil
 }
